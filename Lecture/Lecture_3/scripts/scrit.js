385a353bc90b288b90c61ed385e5f0e286e542ba
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-07-28 11:25:18 +0300

    Перенёс скрипты в отдельный файл, но многое пока не понятно

---
+++ Lecture/Lecture_3/scripts/scrit.js
@@ -0,0 +1,45 @@
+let name = "Привет " + prompt("Введите ваше имя")
+alert(name)
+
+// let first = parseInt(prompt("Введите первое число"))
+// let second = parseInt(prompt("Введите второе число"))
+// alert(first + second)
+
+// Более длинный синтаксис:
+
+// let year = prompt("Какой сейчас год?")
+
+// if (year == 2022) {
+//     alert("Да вы знаток!")
+// } else {
+//     alert("А вот и нет, гы-гы)")
+// }
+
+// Принцип действия одинаковый, с предыдущим блоком кода, но более короткий синтаксис:
+
+// year = prompt("Какой сейчас год?")
+// let accessAllowed = (year == 2022) ? alert("Да вы знаток!") : alert("А вот и нет, гы-гы)");
+
+// let firstNumber = parseInt(prompt("Введите число"))
+// alert(++first)
+
+// let value = parseInt(prompt("Введите число"))
+// let breaks = value
+// ++value
+// for (let index = 0; index < value; index++) {
+//     switch (index) {
+//         case 3:
+//             alert("OOO, that's three!")
+//             break;
+//         case 1:
+//             alert("OOO, that's one")
+//         default:
+//             alert(index)
+//     }
+// }
+
+// Возвращяемый метод
+
+// function name(params) {
+
+// }
